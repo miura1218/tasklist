e24f81d1f89540b29093f6be1c1cafb7d760ac80
miura <miuratoto@gmail.com>
2021-06-21 21:34:26 +0900

    Add flush message

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -47,13 +47,12 @@ public class EditServlet extends HttpServlet {
             request.getSession().setAttribute("message_id", m.getId());
 
             // メッセージIDをセッションスコープに登録
-            if(m != null) {
-                request.getSession().setAttribute("message_id", m.getId());
-            }
+            request.getSession().setAttribute("message_id", m.getId());
 
             RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
             rd.forward(request, response);
     }
     }
-
 }
+
+
