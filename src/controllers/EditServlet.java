6bb927f7dea5f245d28871a6f2e623a11e1d09e5
miura <miuratoto@gmail.com>
2021-06-21 20:20:08 +0900

    Add stylesheets

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -47,7 +47,9 @@ public class EditServlet extends HttpServlet {
             request.getSession().setAttribute("message_id", m.getId());
 
             // メッセージIDをセッションスコープに登録
-            request.getSession().setAttribute("message_id", m.getId());
+            if(m != null) {
+                request.getSession().setAttribute("message_id", m.getId());
+            }
 
             RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
             rd.forward(request, response);
